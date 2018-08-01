4b44a13f707b802218eeea5f0a713a14dfbfab6d
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-01 08:14:33 +0100

    build

--- src/get_url.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-commit=$(git ls-remote git://github.com/robinl/open_data_munge.git | awk '{print $1}' | head -n 1)
-echo https://raw.githubusercontent.com/RobinL/open_data_munge/${commit}/build/open-data-munge.js
