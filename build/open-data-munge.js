18af8203854bcd8086f04a2169e5bbd5fd74ec6f
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-19 09:13:24 +0100

    built

--- build/open-data-munge.js
+++ build/open-data-munge.js
@@ -136,8 +136,13 @@
             return _.map(this.data, d => d[colname])
         };
 
-        this.mutate = function(in_column, out_column, fn) {
+        this.mutate_col = function(in_column, out_column, fn) {
             _.map(this.data, function (d) { d[out_column] = fn(d[in_column]);});
+            return this
+        };
+
+        this.mutate_row = function(in_column, out_column, fn) {
+            this.data = _.map(this.data, function (d) { return fn(d)});
             return this
         };
 
